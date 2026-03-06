e2cade671592a4a8a1ccc67beafb0a675297e9cb
Divnox-01 <ddmsingh1978@gmail.com>
2026-03-06 22:33:05 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,12 +12,10 @@ connectDB();
 
 app.use("/products", productRoutes);
 
-// export the app for Vercel
-module.exports = app;
+// PORT for Render / production
+const PORT = process.env.PORT || 3000;
 
-// run locally only
-if (process.env.NODE_ENV !== "production") {
-  app.listen(3000, () => {
-    console.log("Server running on port 3000");
-  });
-}
+// start server
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
